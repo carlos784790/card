f1a4a23fee121dc0922a0a77c71fddebaba6413d
carlos784790 <136924728+carlos784790@users.noreply.github.com>
2023-11-03 22:19:00 +0000

    done

--- src/app.js
+++ src/app.js
@@ -9,3 +9,48 @@ window.onload = function() {
   //write your code here
   console.log("Hello Rigo from the console!");
 };
+
+window.onload = function() {
+  //write your code here
+
+  function generateRandomCard() {
+    const suits = ["♥", "♠", "♣", "♦"];
+    const values = [
+      "2",
+      "3",
+      "4",
+      "5",
+      "6",
+      "7",
+      "8",
+      "9",
+      "10",
+      "K",
+      "Q",
+      "J",
+      "A"
+    ];
+
+    const randomSuit = suits[Math.floor(Math.random() * suits.length)];
+    const randomValue = values[Math.floor(Math.random() * values.length)];
+
+    const color = randomSuit === "♥" || randomSuit === "♦" ? "red" : "black";
+
+    console.log("Random Suit: ", randomSuit);
+    console.log("Random Value: ", randomValue);
+
+    document.getElementById("top-left").style.color = color;
+    document.getElementById("bottom-right").style.color = color;
+    document.getElementById("card-value").style.color = color;
+
+    document.getElementById("top-left").innerHTML = randomSuit;
+    document.getElementById("bottom-right").innerHTML = randomSuit;
+    document.getElementById("card-value").innerHTML = randomValue;
+  }
+
+  generateRandomCard();
+
+  document
+    .getElementById("generate-btn")
+    .addEventListener("click", generateRandomCard);
+};
